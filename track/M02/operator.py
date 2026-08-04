b02856c91eceace3676cfb3caa66257ca0a046ba
vaishu <vgvg181206@gamil.com>
2026-08-04 17:06:25 +0530

    Add operator.py

---
+++ track/M02/operator.py
@@ -0,0 +1,20 @@
+a=10
+b=20
+
+# write a python code that reads two integers , calculate the sum and display the output
+#read values
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+#calculate the sum
+sum=a+b
+#display the output
+print(f"sum a and b is: {sum}")
+
+# write a python code that reads two integers , calculate the sum and display the output
+a=10
+b=20
+print(f"sum a and b is:{a+b}")
+print("difference of a and b is:" ,a-b)
+print("product of a and b is:" ,a*b)
+print("quotient of a and b is:" ,a/b)
+print("modulus of a and b is:" ,a%b)

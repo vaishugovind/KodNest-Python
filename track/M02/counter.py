40914adc26a081c519cc662183c963ce3220c787
vaishu <vgvg181206@gamil.com>
2026-08-05 16:27:30 +0530

    Add counter.py

---
+++ track/M02/counter.py
@@ -0,0 +1,14 @@
+#read values
+n=int(input("Enter the number: "))
+
+#initialize the count and total
+count=1
+total=0
+
+#calculate sum 
+while count<=n:
+    total=total+count
+    count=count+1
+#diaplay the total
+print(total) 
+

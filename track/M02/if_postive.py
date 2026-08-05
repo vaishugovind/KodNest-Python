61e7c3f4b2a338b1f264b22feaebd330730252d5
vaishu <vgvg181206@gamil.com>
2026-08-05 15:21:24 +0530

    Add if_positive

---
+++ track/M02/if_postive.py
@@ -0,0 +1,10 @@
+# read number
+num=int(input("Enter the number: "))
+
+#checking the positive negative and netural values
+if num >0:
+    print("Number is positive")
+elif num<0:
+    print("Number is negative")
+else:
+    print("Number is zero")

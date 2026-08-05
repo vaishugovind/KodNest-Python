3e66330e031b3e70a419cf76935eb77e94cb0602
vaishu <vgvg181206@gamil.com>
2026-08-05 17:10:54 +0530

    Add numchar

---
+++ track/M02/numchar.py
@@ -0,0 +1,11 @@
+#read the N value and char value
+N=int(input())
+char=input()
+#print the number sequence
+print("Numbers sequence")
+for i in range(1,N+1):
+    print(i)
+#print the char sequence
+print("Char sequence")
+for i in char:
+    print(i)

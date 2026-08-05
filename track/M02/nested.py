30eae092f6738cbbb56b2e5bf7c27de962fe4dc9
vaishu <vgvg181206@gamil.com>
2026-08-05 15:33:32 +0530

    Add nusted.py

---
+++ track/M02/nested.py
@@ -0,0 +1,10 @@
+username="giri"
+password="Giri@123"
+
+if username=="giri":
+    if password=="giri@123":
+        print("valid username and password")
+    else:
+        print("invalid password")
+else:
+    print("invalid username")

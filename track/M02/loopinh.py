e3e303ea375babcb81dda29724b24d6843164364
vaishu <vgvg181206@gamil.com>
2026-08-06 14:36:35 +0530

    looping

---
+++ track/M02/loopinh.py
@@ -0,0 +1,8 @@
+for i in range(5):
+ print(i) #0 1 2 3 4 
+#range (start, stop)
+for i in range(2,6):
+    print(i) #2 3 4 5
+#range(start, stop, step)
+for i in range(2, 7 ,2):
+    print(i) #2 4 6 
